c8d4a0d33e7b8a5efeafc2765e2a0a809098b36f
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-07-01 17:03:44 +0200

    1.0.0-beta

---
+++ DesignPatterns/Creational/Singleton/08 - OperationalSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public sealed class OperationalSingleton : DesignPattern, IDesignPattern
+    {
+        private static OperationalSingleton instance = null;
+        private static int numberOfOperation = 0;
+        private const int refreshedOperation = 100;
+        private OperationalSingleton()
+        {
+            numberOfOperation = 0;
+            Console.WriteLine("instance of an object " + this.GetMyName());
+        }
+        public static OperationalSingleton Instance
+        {
+            get
+            {
+                Interlocked.Add(ref numberOfOperation, 1);
+                if (instance == null || numberOfOperation > refreshedOperation)
+                {
+                    instance = new OperationalSingleton();
+                }
+                Console.WriteLine("Get Instance");
+                return instance;
+            }
+        }
+    }
+}
