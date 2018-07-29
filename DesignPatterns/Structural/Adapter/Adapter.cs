c00d6777f145fe9eaf8f40c43af497405ea31b93
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-07-29 19:50:33 +0200

    beta-1.0.0

---
+++ DesignPatterns/Structural/Adapter/Adapter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Adapter
+{
+    public class Adapter : DesignPattern, IDesignPattern
+    {
+        public static Adapter Instance
+        {
+            get
+            {
+                Customer c = new Customer() { TokenID = "8293jmkm0", StartTime = DateTime.Now };
+                Console.WriteLine("Old integration of Customer: " + c.ToString());
+                DetailedCustomer cc = new DetailedCustomer() { TokenID = "8293jmkm0", StartTime = DateTime.Now };
+                Console.WriteLine("Adapting integration of Customer: " + cc.ToString());
+                return null;
+            }
+        }
+    }
+    //target class that probabily is used in another part of software, but with our smart we think that there's a possibility to reuse in this case.
+    //How can we do it? With Adapter Design Pattern. It's easy.
+    public class Customer
+    {
+        public string TokenID { get; set; }
+        public DateTime StartTime { get; set; }
+        public override string ToString()
+        {
+            return "User: " + this.TokenID + " at " + this.StartTime.ToString();
+        }
+    }
+    //adapter class
+    public class DetailedCustomer : Customer
+    {
+        public DetailOfCustomer DetailOfCustomer { get; set; }
+        public DetailedCustomer()
+        {
+            this.DetailOfCustomer = new DetailOfCustomer(this.TokenID);
+        }
+        public override string ToString()
+        {
+            return "User: " + this.TokenID + " at " + this.StartTime.ToString() + " with Name: " + this.DetailOfCustomer.Name + " and Surname: " + this.DetailOfCustomer.Surname;
+        }
+    }
+    //adaptee class
+    public class DetailOfCustomer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DetailOfCustomer(string tokenID)
+        {
+            SetDataFromDatabase(tokenID);
+        }
+        private void SetDataFromDatabase(string tokenID)
+        {
+            //get data from DB, in this example I set a default value
+            //getDataFromDB(tokenID); //example of call to DB
+            this.Name = "Keyser";
+            this.Surname = "Soze";
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Bridge/Bridge.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Bridge
+{
+    public class Bridge : DesignPattern, IDesignPattern
+    {
+        public static Bridge Instance
+        {
+            get
+            {
+                VideoResource videoResource = new VideoResource();
+                ImageResource imageResource = new ImageResource();
+                BigView bigView = new BigView(videoResource);
+                Console.WriteLine("Big View of Video: " + bigView.Show());
+                bigView = new BigView(imageResource);
+                Console.WriteLine("Big View of Image: " + bigView.Show());
+                LittleView littleView = new LittleView(videoResource);
+                Console.WriteLine("Little View of Video: " + littleView.Show());
+                littleView = new LittleView(imageResource);
+                Console.WriteLine("Little View of Image: " + littleView.Show());
+                return null;
+            }
+        }
+    }
+    //abstraction
+    public abstract class View
+    {
+        public IResource resource;
+        public View(IResource resource)
+        {
+            this.resource = resource;
+        }
+        public abstract string Show();
+    }
+    //refined abstraction
+    public class BigView : View
+    {
+        public BigView(IResource resource) : base(resource)
+        {
+
+        }
+        public override string Show()
+        {
+            return this.resource.Title() + " - " + this.resource.Description() + " - " + this.resource.Image() + " - " + this.resource.Link();
+        }
+    }
+    //second refined abstraction
+    public class LittleView : View
+    {
+        public LittleView(IResource resource) : base(resource)
+        {
+
+        }
+        public override string Show()
+        {
+            return this.resource.Title() + " - " + this.resource.Link();
+        }
+    }
+    //implementor
+    public interface IResource
+    {
+        string Title();
+        string Image();
+        string Description();
+        string Link();
+    }
+    //concrete implementor
+    public class VideoResource : IResource
+    {
+        private Video video;
+        public VideoResource()
+        {
+            this.video = new Video() { Title = "Usual Suspect", Description = "A big movie", Thumbnail = "image.jpg", ContentUrl = "video.mpeg" };
+        }
+        public string Description()
+        {
+            return this.video.Description;
+        }
+
+        public string Image()
+        {
+            return this.video.Thumbnail;
+        }
+
+        public string Link()
+        {
+            return this.video.ContentUrl;
+        }
+
+        public string Title()
+        {
+            return this.video.Title;
+        }
+    }
+    //class for concrete implementor, usually to get data from DB
+    public class Video
+    {
+        public string Title { get; set; }
+        public string Thumbnail { get; set; }
+        public string Description { get; set; }
+        public string ContentUrl { get; set; }
+    }
+    //second concrete implementor
+    public class ImageResource : IResource
+    {
+        private Image image;
+        public ImageResource()
+        {
+            this.image = new Image() { Title = "Usual Suspect Background", Thumbnail = "image.jpg", ContentUrl = "image.png" };
+        }
+        public string Description()
+        {
+            return "";
+        }
+
+        public string Image()
+        {
+            return this.image.Thumbnail;
+        }
+
+        public string Link()
+        {
+            return this.image.ContentUrl;
+        }
+
+        public string Title()
+        {
+            return this.image.Title;
+        }
+    }
+    //class for concrete implementor, usually to get data from DB
+    public class Image
+    {
+        public string Title { get; set; }
+        public string Thumbnail { get; set; }
+        public string ContentUrl { get; set; }
+    }
+}
