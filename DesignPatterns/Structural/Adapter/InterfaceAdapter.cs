379140a615c96aa42d74767ad12500b247cf8b41
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-07-29 21:09:37 +0200

    beta-1.0.0

---
+++ DesignPatterns/Structural/Adapter/InterfaceAdapter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Adapter
+{
+    public class InterfaceAdapter : DesignPattern, IDesignPattern
+    {
+        public static InterfaceAdapter Instance
+        {
+            get
+            {
+                IMessage message = new Message(new SpecialMessage()) { Text = "Hello!!!" };
+                message.Send();
+                return null;
+            }
+        }
+    }
+    public interface IMessage
+    {
+        void Send();
+    }
+    public class Message : IMessage
+    {
+        private SpecialMessage specialMessage;
+        public string Text { get; set; }
+        public Message(SpecialMessage specialMessage)
+        {
+            this.specialMessage = specialMessage;
+        }
+        public void Send()
+        {
+            this.specialMessage.SpecialSend(this.Text);
+        }
+    }
+    public class SpecialMessage
+    {
+        public void SpecialSend(string text)
+        {
+            Console.WriteLine("Special: " + text);
+        }
+    }
+}
