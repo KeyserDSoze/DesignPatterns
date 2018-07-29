b1cff48248af80a4dfb8fe1c85ab786e360432ea
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-07-29 23:57:54 +0200

    beta-1.0.0

---
+++ DesignPatterns/Structural/Decorator/SimpleDecorator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Decorator
+{
+    public class SimpleDecorator : DesignPattern, IDesignPattern
+    {
+        public static SimpleDecorator Instance
+        {
+            get
+            {
+                IIceCream iceCream = new SourCream(new SourCream(new Chocolate()));
+                Console.WriteLine("The cost of an IcreCream with Chocolate and double portion of Sour Cream is: " + iceCream.Cost().ToString());
+                iceCream = new SourCream(new Cream());
+                Console.WriteLine("The cost of an IcreCream with Cream and a portion of Sour Cream is: " + iceCream.Cost().ToString());
+                return null;
+            }
+        }
+    }
+    public interface IIceCream
+    {
+        int Cost();
+    }
+    public class Chocolate : IIceCream
+    {
+        public int Cost()
+        {
+            return 2;
+        }
+    }
+    public class Cream : IIceCream
+    {
+        public int Cost()
+        {
+            return 1;
+        }
+    }
+    //decorator
+    public interface IDecoratorIceCream : IIceCream
+    {
+        int Cost();
+    }
+    //Implementation of Decorator
+    public class SourCream : IDecoratorIceCream
+    {
+        private IIceCream iceCream;
+        public SourCream(IIceCream iceCream)
+        {
+            this.iceCream = iceCream;
+        }
+        public int Cost()
+        {
+            return 1 + this.iceCream.Cost();
+        }
+    }
+}
