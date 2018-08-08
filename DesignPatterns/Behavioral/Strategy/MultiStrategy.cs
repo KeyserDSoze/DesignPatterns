164e07e3591ed84b021444b580e3c2c1850e4b6e
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-08-08 22:20:39 +0200

    beta-1.0.0

---
+++ DesignPatterns/Behavioral/Strategy/MultiStrategy.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.MultiStrategy
+{
+    public class MultiStrategy : DesignPattern, IDesignPattern
+    {
+        public static MultiStrategy Instance
+        {
+            get
+            {
+                Cat normalCat = new Cat(new SmallJump(), new NormaEat(), new NormalMeow());
+                normalCat.Show();
+                Cat superCat = new Cat(new BigJump(), new NormaEat(), new Roar());
+                superCat.Show();
+                return null;
+            }
+        }
+    }
+    public interface IJumping
+    {
+        void Jump();
+    }
+    public interface IMeowing
+    {
+        void Meow();
+    }
+    public interface IEating
+    {
+        void Eat();
+    }
+    public class SmallJump : IJumping
+    {
+        public void Jump()
+        {
+            Console.WriteLine("Small Jump");
+        }
+    }
+    public class BigJump : IJumping
+    {
+        public void Jump()
+        {
+            Console.WriteLine("Big Jump");
+        }
+    }
+    public class NormalMeow : IMeowing
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Normal Meow");
+        }
+    }
+    public class Roar : IMeowing
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Roar");
+        }
+    }
+    public class NormaEat : IEating
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Normal Eat");
+        }
+    }
+    public class Cat
+    {
+        public IJumping Jumping;
+        public IEating Eating;
+        public IMeowing Meowing;
+        public Cat(IJumping jumping, IEating eating, IMeowing meowing)
+        {
+            this.Jumping = jumping;
+            this.Eating = eating;
+            this.Meowing = meowing;
+        }
+        public void Show()
+        {
+            this.Jumping.Jump();
+            this.Eating.Eat();
+            this.Meowing.Meow();
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Strategy/Strategy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Strategy
+{
+    public class Strategy : DesignPattern, IDesignPattern
+    {
+        public static Strategy Instance
+        {
+            get
+            {
+                Client client = new Client(new NormalBehavior());
+                client.Run();
+                Client superClient = new Client(new SuperBehavior());
+                client.Run();
+                return null;
+            }
+        }
+    }
+    public interface IBehavior
+    {
+        void DoSomething();
+    }
+    public class NormalBehavior : IBehavior
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Normal");
+        }
+    }
+    public class SuperBehavior : IBehavior
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Super");
+        }
+    }
+    public class Client
+    {
+        public IBehavior Behavior;
+        public Client(IBehavior behavior)
+        {
+            this.Behavior = behavior;
+        }
+
+        public void Run()
+        {
+            this.Behavior.DoSomething();
+        }
+    }
+    //public class Client: IBehavior
+    //{
+    //    public IBehavior Behavior;
+    //    public Client(IBehavior behavior)
+    //    {
+    //        this.Behavior = behavior;
+    //    }
+
+    //    public void DoSomething()
+    //    {
+    //        this.Behavior.DoSomething();
+    //    }
+    //}
+}
