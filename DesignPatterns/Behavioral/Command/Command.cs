834dc55da8013670549b537b42d93ee6305effca
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-08-09 22:32:24 +0200

    beta-1.0.0

---
+++ DesignPatterns/Behavioral/Command/Command.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Command
+{
+    public class Command : DesignPattern, IDesignPattern
+    {
+        public static Command Instance
+        {
+            get
+            {
+                Client client = new Client();
+                client.Compute(100);
+                client.Compute(20);
+                client.Compute(40);
+                client.Undo();
+                client.Undo();
+                client.Redo();
+                client.Compute(100);
+                return null;
+            }
+        }
+    }
+    public abstract class ACommand
+    {
+        public abstract void Execute();
+        public abstract void UnExecute();
+    }
+    public class CalculatorKeyboard : ACommand
+    {
+        private CalculatorCpu calculatorCpu;
+        private int value;
+        public CalculatorKeyboard(CalculatorCpu calculatorCpu, int value)
+        {
+            this.calculatorCpu = calculatorCpu;
+            this.value = value;
+        }
+        public override void Execute()
+        {
+            this.calculatorCpu.Plus(this.value);
+        }
+        public override void UnExecute()
+        {
+            this.calculatorCpu.Minus(this.value);
+        }
+    }
+    public class CalculatorCpu
+    {
+        private int current = 0;
+        public void Plus(int value)
+        {
+            this.current += value;
+            Console.WriteLine("value now: " + this.current.ToString());
+        }
+        public void Minus(int value)
+        {
+            this.current -= value;
+            Console.WriteLine("value now: " + this.current.ToString());
+        }
+    }
+    public class Client
+    {
+        private List<ACommand> aCommands = new List<ACommand>();
+        private CalculatorCpu calculatorCpu = new CalculatorCpu();
+        private int currentPosition = 0;
+        public void Compute(int value)
+        {
+            ACommand aCommand = new CalculatorKeyboard(this.calculatorCpu, value);
+            aCommand.Execute();
+            this.aCommands = this.aCommands.Take(this.currentPosition).ToList();
+            this.aCommands.Add(aCommand);
+            this.currentPosition++;
+        }
+        public void Undo()
+        {
+            if (this.currentPosition == 0) return;
+            this.aCommands[--this.currentPosition].UnExecute();
+        }
+        public void Redo()
+        {
+            if (this.currentPosition + 1 >= this.aCommands.Count) return;
+            this.aCommands[++this.currentPosition].Execute();
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.TemplateMethod
+{
+    public class TemplateMethod : DesignPattern, IDesignPattern
+    {
+        public static TemplateMethod Instance
+        {
+            get
+            {
+                AbstractSubscription mySubscription = new MySubscription();
+                mySubscription.MakeSubscription();
+                AbstractSubscription anotherSubscription = new AnotherSubscription();
+                anotherSubscription.MakeSubscription();
+                return null;
+            }
+        }
+    }
+    public abstract class AbstractSubscription
+    {
+        public void MakeSubscription()
+        {
+            this.Activate();
+            this.Notify();
+            this.Bill();
+        }
+        public abstract void Activate();
+        public abstract void Notify();
+        public abstract void Bill();
+    }
+    public class MySubscription : AbstractSubscription
+    {
+        public override void Activate()
+        {
+            Console.WriteLine("Make Activation");
+        }
+
+        public override void Bill()
+        {
+            Console.WriteLine("Make Billing");
+        }
+
+        public override void Notify()
+        {
+            Console.WriteLine("Send Notification");
+        }
+    }
+    public class AnotherSubscription : AbstractSubscription
+    {
+        public override void Activate()
+        {
+            Console.WriteLine("Make another Activation");
+        }
+
+        public override void Bill()
+        {
+            Console.WriteLine("Make another Billing");
+        }
+
+        public override void Notify()
+        {
+            Console.WriteLine("Send another Notification");
+        }
+    }
+}
