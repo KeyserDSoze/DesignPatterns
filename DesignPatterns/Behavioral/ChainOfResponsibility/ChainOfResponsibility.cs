1bbaf7d539cab357f26d6e9091668262dd00ac62
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-08-11 22:25:11 +0200

    Beta-1.0.0

---
+++ DesignPatterns/Behavioral/ChainOfResponsibility/ChainOfResponsibility.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.ChainOfResponsibility
+{
+    public class ChainOfResponsibility : DesignPattern, IDesignPattern
+    {
+        public static ChainOfResponsibility Instance
+        {
+            get
+            {
+                //here we're creating the chain for our special case
+                ISubscriptionEvent activation = new Activation();
+                ISubscriptionEvent message = new Message();
+                ISubscriptionEvent billing = new Billing();
+                activation.SetNextSubscriptionEvent(message);
+                message.SetNextSubscriptionEvent(billing);
+
+                //and here we execute the chain
+                CustomerBase customerBase = new CustomerBase(activation) { UserId = "KeyserDSoze" };
+                Console.WriteLine(customerBase.ToString());
+                //activation.Execute(customerBase, SubscriptionEventType.activation); 
+                //usually it's started from the first chain, but i want to make a more efficient starting
+                //with dependecy injection in customerbase constructor we decide what starts the chain
+                customerBase.Execute(SubscriptionEventType.activation);
+                Console.WriteLine(customerBase.ToString());
+                customerBase.Execute(SubscriptionEventType.sendMessage);
+                Console.WriteLine(customerBase.ToString());
+                customerBase.Execute(SubscriptionEventType.bill);
+                Console.WriteLine(customerBase.ToString());
+
+                return null;
+            }
+        }
+    }
+    public enum SubscriptionEventType
+    {
+        activation,
+        sendMessage,
+        bill
+    }
+    public class CustomerBase
+    {
+        public string UserId { get; set; }
+        public bool IsActive { get; set; } = false;
+        public bool InformativeMessageIsSent { get; set; } = false;
+        public double Billed { get; set; } = 0;
+        private ISubscriptionEvent startEvent;
+        public CustomerBase(ISubscriptionEvent startEvent)
+        {
+            this.startEvent = startEvent;
+        }
+        public void Execute(SubscriptionEventType subscriptionEventType)
+        {
+            this.startEvent.Execute(this, subscriptionEventType);
+        }
+        public override string ToString()
+        {
+            return $"User: {this.UserId} is active: {this.IsActive}, the system sent informative message: {this.InformativeMessageIsSent} and is billed for {this.Billed}";
+        }
+    }
+    public interface ISubscriptionEvent  //chain
+    {
+        void SetNextSubscriptionEvent(ISubscriptionEvent subscriptionEvent);
+        void Execute(CustomerBase customerBase, SubscriptionEventType subscriptionEventType);
+    }
+    public class Activation : ISubscriptionEvent
+    {
+        private ISubscriptionEvent nextSubscriptionEvent;
+        public void Execute(CustomerBase customerBase, SubscriptionEventType subscriptionEventType)
+        {
+            if (subscriptionEventType == SubscriptionEventType.activation)
+            {
+                customerBase.IsActive = true;
+            }
+            else
+            {
+                //it's possible to implement a check, for example go on next one only if the user is active in this case
+                this.nextSubscriptionEvent.Execute(customerBase, subscriptionEventType);
+            }
+        }
+
+        public void SetNextSubscriptionEvent(ISubscriptionEvent subscriptionEvent)
+        {
+            this.nextSubscriptionEvent = subscriptionEvent;
+        }
+    }
+    public class Message : ISubscriptionEvent
+    {
+        private ISubscriptionEvent nextSubscriptionEvent;
+        public void Execute(CustomerBase customerBase, SubscriptionEventType subscriptionEventType)
+        {
+            if (subscriptionEventType == SubscriptionEventType.sendMessage)
+            {
+                customerBase.InformativeMessageIsSent = true;
+            }
+            else
+            {
+                this.nextSubscriptionEvent.Execute(customerBase, subscriptionEventType);
+            }
+        }
+
+        public void SetNextSubscriptionEvent(ISubscriptionEvent subscriptionEvent)
+        {
+            this.nextSubscriptionEvent = subscriptionEvent;
+        }
+    }
+    public class Billing : ISubscriptionEvent
+    {
+        private ISubscriptionEvent nextSubscriptionEvent;
+        public void Execute(CustomerBase customerBase, SubscriptionEventType subscriptionEventType)
+        {
+            if (subscriptionEventType == SubscriptionEventType.bill)
+            {
+                customerBase.Billed = 6;
+            }
+            else
+            {
+                this.nextSubscriptionEvent.Execute(customerBase, subscriptionEventType);
+            }
+        }
+
+        public void SetNextSubscriptionEvent(ISubscriptionEvent subscriptionEvent)
+        {
+            this.nextSubscriptionEvent = subscriptionEvent;
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Interpreter/Interpreter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Interpreter
+{
+    public class Interpreter : DesignPattern, IDesignPattern
+    {
+        public static Interpreter Instance
+        {
+            get
+            {
+                Client client = new Client() { Formula = "C5H8" };
+                List<IAtom> atomsToCheck = new List<IAtom>();
+                atomsToCheck.Add(new Hydrogen());
+                atomsToCheck.Add(new Carbon());
+                double totalMass = 0;
+                foreach (IAtom atom in atomsToCheck)
+                {
+                    totalMass += atom.Convert(client);
+                }
+                Console.WriteLine("Total Weight of C5H8: " + totalMass.ToString());
+                return null;
+            }
+        }
+    }
+    public class Client
+    {
+        public string Formula { get; set; }
+    }
+    public interface IAtom
+    {
+        double Convert(Client client);
+    }
+    public class Hydrogen : IAtom
+    {
+        public double Convert(Client client)
+        {
+            if (client.Formula.ToLower().Contains("h"))
+            {
+                Regex regex = new Regex("h[0-9]*"); //to get how many atoms of hydrogen it's composed
+                if (regex.IsMatch(client.Formula.ToLower()))
+                {
+                    return int.Parse(regex.Match(client.Formula.ToLower()).Value.ToLower().Replace("h", "")) * 1.007;
+                }
+                return 1.007;
+            }
+            return 0;
+        }
+    }
+    public class Carbon : IAtom
+    {
+        public double Convert(Client client)
+        {
+            if (client.Formula.ToLower().Contains("c")) {
+                Regex regex = new Regex("c[0-9]*"); //to get how many atoms of carbon it's composed
+                if (regex.IsMatch(client.Formula.ToLower()))
+                {
+                    return int.Parse(regex.Match(client.Formula.ToLower()).Value.ToLower().Replace("c", "")) * 12;
+                }
+                return 12;
+            }
+            return 0;
+        }
+    }
+}
